8d5a8a99c83e5f4b0a4514370ba161ea5aa6abed
Mark Bence Kiss <31191974+bassexplorer@users.noreply.github.com>
2020-06-02 00:17:58 +0200

    start: Header & Nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,2 +1,11 @@
 // It is the script file for the navigation.
 const navBox = document.getElementsByClassName('nav-container')[0];
+let previousPage = document.getElementsByClassName("active");
+
+window.addEventListener('hashchange', ()=>{
+
+    const currentPage = window.location.hash.substr(1);
+    const activeNavItem = navBox.querySelector(`[href="#${currentPage}"]`);
+    previousPage[0].classList.toggle("active");
+    activeNavItem.classList.toggle("active");
+});
